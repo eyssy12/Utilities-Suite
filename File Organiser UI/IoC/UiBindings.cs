3e9b7be94af6424d04416e10fe9d5e695f5df705
Roberts Zagorskis <zagorskis.roberts@yahoo.com>
2016-11-12 12:27:06 +0000

    More untracked files

---
+++ File Organiser UI/IoC/UiBindings.cs
@@ -0,0 +1,42 @@
+﻿namespace File.Organiser.UI.IoC
+{
+    using System.Windows.Controls;
+    using Controls;
+    using EyssyApps.Configuration.Library;
+    using SimpleInjector;
+
+    public class UiBindings : CommonBindings
+    {
+        protected override void LoadBindings()
+        {
+            base.LoadBindings();
+
+            this.BindWindows();
+            this.BindViews();
+            this.BindControls();
+        }
+
+        protected virtual void BindWindows()
+        {
+            this.Bind<IMainWindow, MainWindow>();
+        }
+
+        protected virtual void BindViews()
+        {
+            // TODO: bind views
+        }
+
+        protected virtual void BindControls()
+        {
+            this.Bind<ISystemTrayControl>(container =>
+            {
+                ContextMenu menu = App.Current.TryFindResource(App.ControlTrayContextMenu) as ContextMenu;
+
+                return new SystemTrayControl(
+                    menu,
+                    UiResources.App,
+                    App.Name);
+            }, Lifestyle.Singleton);
+        }
+    }
+}
